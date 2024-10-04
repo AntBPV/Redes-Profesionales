f66995952a6687bf74103d1aeb23f3ac32923f17
AntonhyBPV <antonio.parra@campusucc.edu.co>
2024-10-04 10:36:54 -0500

    Repository-created

---
+++ redesProfesionales/post/serializer.py
@@ -0,0 +1,7 @@
+from post.models import PostModel
+from rest_framework import serializers
+
+class post_serializer(serializers.ModelSerializer):
+    class Meta:
+        model=PostModel
+        fields=['id','user','title','text','slug']
