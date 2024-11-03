c9ddf1e582c35b2435b105f007c83569d22ae3b8
AntonhyBPV <antonio.parra@campusucc.edu.co>
2024-11-03 12:50:05 -0500

    changed post model

--- BackEnd/post/serializer.py
+++ BackEnd/post/serializer.py
@@ -1,8 +1,14 @@
 from post.models import PostModel
+from UserProfile.models import Profile
 from rest_framework import serializers
 
 
 class post_serializer(serializers.ModelSerializer):
+    profile = serializers.PrimaryKeyRelatedField(
+        queryset = Profile.objects.all(),
+        required = True
+    )
+    
     class Meta:
         model=PostModel
-        fields=['id','user','image','text']
+        fields=['id','user', 'profile', 'image', 'text']
