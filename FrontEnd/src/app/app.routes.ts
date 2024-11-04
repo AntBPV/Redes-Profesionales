72ecf8ada695722a09f783c786cd04908f240c89
cristhianDavidOrbes <134344774+cristhianDavidOrbes@users.noreply.github.com>
2024-11-03 19:08:04 -0500

    frontend actualizar

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import {InicioComponent} from './pagina-redes-profesionales/inicio/inicio.component';
+import {IniciarSesionComponent} from './pagina-redes-profesionales/iniciar-sesion/iniciar-sesion.component';
+import { Component } from '@angular/core';
+import {RegistrarseComponent} from './pagina-redes-profesionales/registrarse/registrarse.component';
 
-export const routes: Routes = [];
+import { InicioPerfilComponent } from './pagina-redes-profesionales/inicio-perfil/inicio-perfil.component';
+export const routes: Routes = [
+    {
+        path:'',
+        component:InicioComponent
+    },
+    {
+        path:'iniciarSesion',
+        component:IniciarSesionComponent
+    },
+    {
+        path:'Registrarse',
+        component:RegistrarseComponent
+    },
+
+    {   path: 'publicacion',
+        component:InicioPerfilComponent 
+    },
+
+
+];
